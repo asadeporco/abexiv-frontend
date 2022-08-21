1d946e2da77f933e11f35c8db52b318b358879ed
Marcelino Jannuzzi <jannuzzimarcelino1@gmail.com>
2022-08-21 17:20:47 -0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx 👍</code> and save to reload.
+          Edit <code>src/App.tsx 👌👌👌</code> and save to reload.
         </p>
         <a
           className="App-link"
