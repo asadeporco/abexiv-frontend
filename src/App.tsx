c9306543969f0a0d73f58098479d4b39d50183c2
Marcelino Jannuzzi <marcelino.jannuzzi@agriness.com>
2022-08-22 22:45:08 -0300

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,24 @@ import { theme } from './global/styles/theme'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-       <UserRoutes />
-    </ThemeProvider>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            👌It works 👍
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> parent of e91b9a2... pipeline final test
 }
 
 export default App
